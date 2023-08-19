1b3ec8aeee47ff9b66e427282f3a47ceb15db8ec
Pavel Zavartsev <pavel_zavartsev@mail.ru>
2023-08-19 11:13:06 +0300

    'added debug_toolbar and ordering'

--- 2.2-databases-2/orm_migrations/school/urls.py
+++ 2.2-databases-2/orm_migrations/school/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
 from school.views import students_list
+from website import settings
 
-urlpatterns = [
-    path('', students_list, name='students'),
-]
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+        path('', students_list, name='students'),
+    ]
--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -6,15 +6,14 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    object_list = Student.objects.all()
-    print(object_list)
-
+    object_list = Student.objects.order_by('group')
+    # object_list = Student.objects.all()
     context = {
         'object_list': object_list
     }
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
+    # ordering = 'group'
 
     return render(request, template, context)
