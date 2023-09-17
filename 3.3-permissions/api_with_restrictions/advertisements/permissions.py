a28971b945f5b45187fdb6142b7ab2f34ba7862d
Pavel Zavartsev <pavel_zavartsev@mail.ru>
2023-09-18 00:11:15 +0300

    'fixed class IsOwnerOrReadOnly'

--- 3.3-permissions/api_with_restrictions/advertisements/permissions.py
+++ 3.3-permissions/api_with_restrictions/advertisements/permissions.py
@@ -5,7 +5,7 @@ class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         print(f'user is: {request.user.username}')
         print(f'user is: {request.user.username == "admin"}')
-        if request.method == 'GET' or request.user.username == 'admin' or request.user.username == 'admin2':
+        if request.method == 'GET' or request.user.is_staff:
             return True
         return request.user == obj.creator
 
